494f00c3e516a2588ca28de7581142b41069cc2d
SheeyhnChyoh <heven006@126.com>
2016-03-08 09:48:06 +0800

    Initial Commit

---
+++ obayMyc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  obayMyc
+//
+//  Created by Longcai on 16/3/8.
+//  Copyright (c) 2016年 Longcai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
